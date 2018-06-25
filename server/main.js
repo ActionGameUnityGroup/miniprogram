f35a76f03da0ed2ff88aafac5fb769befd0ea7d9
reriliv <reri_liv@163.com>
2018-06-25 11:00:50 +0800

    change server port

--- server/main.js
+++ server/main.js
@@ -6,5 +6,5 @@ const zlib = require('zlib');
 appFunction(app, __dirname);
 
 app.listen(9000, () => {
-  console.log('Server running at http://127.0.0.1:3000');
+  console.log('Server running at http://127.0.0.1:9000');
 });
