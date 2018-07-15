fc715eaeee61a71bc4e284b48d15f4c17e08e4f0
HakuHayabusa <bh1981314@gmail.com>
2018-07-15 20:15:27 +0800

    update comment

--- client/ScoreMarket/GoodsPage/goods.js
+++ client/ScoreMarket/GoodsPage/goods.js
@@ -22,7 +22,12 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+  wx.setNavigationBarTitle({
+    title: '积分商城',
+    success: function(res) {},
+    fail: function(res) {},
+    complete: function(res) {},
+  })
   },
 
   /**
