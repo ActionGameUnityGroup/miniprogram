295a663fad8fb4152001f9e21db8eb9a37308f35
reriliv <www.reri_liv@163.com>
2018-09-21 01:01:04 +0800

    add custom api

--- server/controllers/custom/custom.js
+++ server/controllers/custom/custom.js
@@ -6,7 +6,7 @@ const WXBizMsgCrypt = require('wechat-crypto');
 
 class Custom {
 
-  async getAdminList(ctx){
+  async getUserMessage(ctx){
     let params = ctx.query;
     // let data = await customModel.find(query, '-_id');
     const [token, appid, encodingaeskey] = ['changdao2018', "wxba59a2c0824fd1db", '2NViDJCTlFv2IGxFrn4jQJJFWhSlFqqdHlXHw45pgH2'];
@@ -26,24 +26,6 @@ class Custom {
     console.log(data, 'data');
     ctx.body = data.message;
     // ctx.type = 'text/json';
-  }
-
-  async getUserMessage(ctx){
-      // let params = getParams(ctx);
-      let params = ctx.query;
-      console.log(params, '微信');
-      ctx.body = await formatData({info: '成功'});
-      /*.then((res) => {
-        console.log(res, 50);
-        const data = formatData(res);
-        ctx.body = data;
-        ctx.type = 'text/json';
-      })
-      .catch((err) => {
-        const error = formatDataFail(err);
-        ctx.body = error;
-        ctx.type = 'text/json';
-      });*/
   }
 
 };
