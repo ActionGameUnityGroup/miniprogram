c2237d4263397da3a386cba9e9d1d397e7b9138c
reriliv <www.reri_liv@163.com>
2018-09-21 00:54:38 +0800

    add custom api

--- server/controllers/custom/custom.js
+++ server/controllers/custom/custom.js
@@ -2,15 +2,29 @@ const {formatData, formatDataFail} = require('../api/formatData');
 const customModel = require('../../models/customModel');
 // const request = require('../../app/request');
 // const WXBizDataCrypt = require('../../app/WXBizDataCrypt');
+const WXBizMsgCrypt = require('wechat-crypto');
 
 class Custom {
 
   async getAdminList(ctx){
-    let query = ctx.query;
-    let data = await customModel.find(query, '-_id');
-    await ctx.info(`${ctx.url}: ${data}`);
-    ctx.body = await formatData(data);
-    ctx.type = 'text/json';
+    let params = ctx.query;
+    // let data = await customModel.find(query, '-_id');
+    const [token, appid, encodingaeskey] = ['changdao2018', "wxba59a2c0824fd1db", '2NViDJCTlFv2IGxFrn4jQJJFWhSlFqqdHlXHw45pgH2'];
+    let [
+      signature,
+      timestamp,
+      nonce,
+      echostr
+    ] = [
+      params.signature,
+      params.timestamp,
+      params.nonce,
+      params.echostr,
+    ];
+    let cryptor = new WXBizMsgCrypt(token, encodingaeskey, appid);
+    let data = cryptor.decrypt(echostr);
+    ctx.body = data.message;
+    // ctx.type = 'text/json';
   }
 
   async getUserMessage(ctx){
