32d07e378b7ad59d6528155f38bcf9b13f35e315
Reriliv <reri_liv@163.com>
2018-09-24 23:38:29 +0800

    update server

--- server/controllers/custom/custom.js
+++ server/controllers/custom/custom.js
@@ -47,40 +47,21 @@ class Custom {
     }
   }
 
-  async achieveUserMessage(ctx){
+  /*async achieveUserMessage(ctx){
     // 获取用户信息
     let params = ctx.query;
     console.log(params, '请求参数');
-    console.log(ctx.request.body, '请求body');
-    // signature  签名
-    // echostr  随机字符串
-    // timestamp  时间戳
-    // nonce  随机数
-
-    // 1. 获取微信推送过来的信息
     let {
       msg_signature,
+      signature,
       echostr,
       timestamp,
       nonce
     } = params;
 
-    // 2. 将token、timestamp、nonce三个参数进行字典排序
-    let array = ['changdao', timestamp, nonce];
-    await array.sort();
+    
 
-    // 3. 将三个参数拼接成一个字符串进行sha1加密
-    const tempStr = array.join('');
-    const hashCode = crypto.createHash('sha1');
-    const resultCode = hashCode.update(tempStr, 'utf8').digest('hex');
-    console.log(resultCode, '加密后的code');
-    // 4. 将加密后的字符串与signature对比，标识请求来源于微信
-    if(resultCode === signature){
-      ctx.body = echostr;
-    } else {
-      ctx.body = {code: -1, data: '验证失败'};
-    }
-  }
+  }*/
 
   // async getUserMessage(ctx){
   //   let params = ctx.query;
--- server/routes/custom/custom.js
+++ server/routes/custom/custom.js
@@ -3,7 +3,7 @@ const Custom = require('../../controllers/custom/custom');
 module.exports = {
   'POST /custom/getUserMessage': async (ctx) => {
     console.log('获取微信用户消息');
-    await Custom.achieveUserMessage(ctx);
+    await Custom.getUserMessage(ctx);
   },
   'GET /custom/getUserMessage': async (ctx) => {
     console.log('微信客服消息');
