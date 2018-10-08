ac67ef9cb9290f71d0f33e5cb609b07c43c4c37d
reri_liv <reri_liv@163.com>
2018-10-08 09:43:36 +0800

    changes static files get method

--- server/app/static-file.js
+++ server/app/static-file.js
@@ -1,14 +1,17 @@
+const fs = require('fs');
+
 module.exports = function(rootPath){
-  return async(ctx, next) => {
+  return (ctx, next) => {
     let URL = ctx.url;
     if (URL.includes('.')&&URL!=='/favicon.ico') {
       // 读取静态文件
       URL = rootPath + '/public' + URL;
       const mime = require('mime');
-      const fs = require('fs');
-      ctx.body = await fs.createReadStream(URL);
-      ctx.type = await mime.getType(URL);
+      console.log('获取文件');
+      // console.log(ctx, 'response body');
+      ctx.body = fs.createReadStream(URL);
+      ctx.type = mime.getType(URL);
     }
-    await next();
+    next();
   };
 };
--- server/controllers/api/Audio.js
+++ server/controllers/api/Audio.js
@@ -1,10 +1,18 @@
+const {formatData} = require('./formatData');
 const upload = require('./upload');
+const path = require('path');
+const fs = require('fs');
 
 class Audio {
+
   async setAudio(ctx){
     console.log('上传文件');
     let fileStream = await upload(ctx);
+    console.log(fileStream, '文件流');
+    // fs.ftruncate(fileStream, )
+    ctx.body = formatData({info: '上传成功'});
   }
+
   async getAudio(ctx){
     // 
   }
