1bb48698fa59a27ec0a55d392a5c6065f5bce78c
reri_liv <reri_liv@163.com>
2018-10-08 09:47:56 +0800

    changes static files get method

--- server/app/static-file.js
+++ server/app/static-file.js
@@ -1,17 +1,15 @@
+const mime = require('mime');
 const fs = require('fs');
 
 module.exports = function(rootPath){
-  return (ctx, next) => {
+  return async(ctx, next) => {
     let URL = ctx.url;
     if (URL.includes('.')&&URL!=='/favicon.ico') {
       // 读取静态文件
       URL = rootPath + '/public' + URL;
-      const mime = require('mime');
-      console.log('获取文件');
-      // console.log(ctx, 'response body');
-      ctx.body = fs.createReadStream(URL);
-      ctx.type = mime.getType(URL);
+      ctx.body = await fs.createReadStream(URL);
+      ctx.type = await mime.getType(URL);
     }
-    next();
+    await next();
   };
 };
