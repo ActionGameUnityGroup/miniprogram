a379a1ee5446b203dcbd4ce40305d04fc7b26c8d
reri_liv <reri_liv@163.com>
2018-10-09 11:16:50 +0800

    changes files

--- client/Health/Health.js
+++ client/Health/Health.js
@@ -1,10 +1,12 @@
 const app = getApp();
+let _this;
 
 Page({
   data: {
     isDetail: false,
     isCourse: true,
     isSeason: false,
+    bannerList: [],
     courseList: [
       {courseImg: '', courseName: '《中庸》第一章：天命之谓性', courseText: '“愤怒、焦虑、悲伤、抱怨......”各种负面情绪时常“充斥”我们的生命，影响我们的生活', currentCourse: 4, coursePrice: 199},
       {courseImg: '', courseName: '《中庸》第一章：天命之谓性', courseText: '“愤怒、焦虑、悲伤、抱怨......”各种负面情绪时常“充斥”我们的生命，影响我们的生活', currentCourse: 4, coursePrice: 199},
@@ -17,13 +19,22 @@ Page({
   },
   onLoad: function(options){
     console.log(options);
+    _this = this;
     wx.setNavigationBarTitle({
       title: '疗愈'
     });
-    let height = app.globalData.systemInfo.windowHeight;
-    console.log(height);
-    this.setData({
-      scrollViewHeight: height - (376+16+90+102)/2
+    wx.request({
+      url: 'https://www.changdaolife.cn/api/banner/getBanner?page=health',
+      method: 'GET',
+      success: function(res){
+        console.log(res.data.requestData[0].bannerList, '回调');
+        let height = app.globalData.systemInfo.windowHeight;
+        console.log(height);
+        _this.setData({
+          scrollViewHeight: height - (376+16+90+102)/2,
+          bannerList: res.data.requestData[0].bannerList
+        });
+      }
     });
   },
   detailNavAction:function(){
