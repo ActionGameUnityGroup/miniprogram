059dcd46a6242c5de6a89906ff9ccffaa28778f0
Reriliv <reri_liv@163.com>
2018-10-12 23:14:11 +0800

    changes files & api

--- server/models/articleModel.js
+++ server/models/articleModel.js
@@ -2,6 +2,7 @@ const {db, mongoose} = require('../mongodb/db');
 
 let articleSchema = new mongoose.Schema({
   articleName: String,
+  articleCover: String,
   imageFrom: String,
   articleFrom: String,
   articleTime: String,
