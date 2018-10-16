d18d0cbff0f380fb5d67a96a824860fca7a8591d
reri_liv <reri_liv@163.com>
2018-10-16 15:46:56 +0800

    changes article api

--- server/controllers/api/Article.js
+++ server/controllers/api/Article.js
@@ -1,13 +1,30 @@
 const articleModel = require('../../models/articleModel');
-const {formatData} = require('./formatData');
+const {formatData, formatDataFail} = require('./formatData');
 
 class Article{
+
+  async getArticle(ctx){
+    const params = ctx.query;
+    console.log(params, '参数');
+    const articleId = params.articleId;
+    try { 
+      const data = await articleModel.find({articleId: articleId}, '-_id');
+      console.log(data[0], '课程');
+      ctx.body = formatData({article: data[0]});
+      ctx.type = 'text/json';
+    } catch(e) {
+      // statements
+      console.log(e);
+      ctx.body = formatDataFail({info: '获取文章错误!'});
+    }
+  }
+
   async getArticleList(ctx){
     const params = ctx.query;
     console.log(params, '参数');
     const index = params.index || 0;
     console.log(index, '下标');
-    const data = await articleModel.find({}, '-_id')
+    const data = await articleModel.find({}, '-_id -articleDetail -imageFrom -articleFrom')
                       .sort({articleTime: -1})
                       .skip(index * 5)
                       .limit(5);
@@ -15,6 +32,7 @@ class Article{
     ctx.body = formatData({articleList: data});
     ctx.type = 'text/json';
   }
+
 }
 
 module.exports = new Article();
--- server/routes/api/article.js
+++ server/routes/api/article.js
@@ -1,6 +1,9 @@
 const Article = require('../../controllers/api/Article');
 
 module.exports = {
+  'GET /api/article/getArticle': async (ctx) => {
+    await Article.getArticle(ctx);
+  },
   'GET /api/article/getArticleList': async (ctx) => {
     await Article.getArticleList(ctx);
   },
