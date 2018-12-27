f11357df2b0a1ab846c70d5d8801539768692cf7
Reriliv <reri_liv@163.com>
2018-12-27 23:10:25 +0800

    update controller

--- server/app/controller/api/v0/user.js
+++ server/app/controller/api/v0/user.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const rootDirectory = path.resolve(__dirname, '../../../');
-const userService = require(`${rootDirectory}/service/v0/userService`);
+// const userService = require(`${rootDirectory}/service/v0/userService`);
 
 class User{
 	async login(ctx){
