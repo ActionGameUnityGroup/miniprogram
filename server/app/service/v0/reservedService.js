c18b7e242fa1c98b6939e6084ed0b2727a9f3571
Reriliv <reri_liv@163.com>
2019-01-13 15:05:30 +0800

    update an order api

--- server/app/service/v0/reservedService.js
+++ server/app/service/v0/reservedService.js
@@ -5,50 +5,45 @@ const formatData = require(`${rootDirectory}/service/formatData`);
 
 class reservedService extends formatData{
 
-	async getReservedList(ctx){
-		let response;
-		try{
-			let data = await reservedModel.find({}, '-_id');
-			response = this.formatDataSuccess(data);
-		} catch(e){
-			response = this.formatDataFail(e.message);
-			ctx.throw(500);
-		}
-		return response;
-	}
+  async getReservedList(ctx){
+    let response;
+    try{
+      let data = await reservedModel.find({}, '-_id');
+      response = this.formatDataSuccess(data);
+    } catch(e){
+      response = this.formatDataFail(e.message);
+      ctx.throw(500);
+    }
+    return response;
+  }
 
-	async setReservedInfo(ctx){
-		let response;
-		try{
-			console.log(ctx.request.body);
-			const { openId, name, mobile, profession, city, email, gender, courseId, } = ctx.request.body;
-			console.log(openId, name, mobile, profession, city, email, gender, courseId);
-			if(!openId || !name || !mobile || !profession || !city || !gender) {
-				console.log('something wrong...');
-				throw new Error('报名失败！请正确填写信息！');
-			}
-			let newReservedInfo = new reservedModel({
-				openId,
-				name,
-				mobile,
-				profession,
-				city,
-				email,
-				gender,
-				courseId,
-			});
-			let res = await newReservedInfo.save();
-			response = this.formatDataSuccess('报名成功！');
-			/*if(res){
-			} else {
-				console.log('something wrong...');
-				throw new Error('报名失败！');
-			}*/
-		} catch(e){
-			response = this.formatDataFail(e.message);
-		}
-		return response;
-	}
+  async setReservedInfo(ctx){
+    let response;
+    try{
+      console.log(ctx.request.body);
+      const { openId, name, mobile, profession, city, email, gender, courseId, } = ctx.request.body;
+      console.log(openId, name, mobile, profession, city, email, gender, courseId);
+      if(!openId || !name || !mobile || !profession || !city || !gender) {
+        console.log('something wrong...');
+        throw new Error('报名失败！请正确填写信息！');
+      }
+      let newReservedInfo = new reservedModel({
+        openId,
+        name,
+        mobile,
+        profession,
+        city,
+        email,
+        gender,
+        courseId,
+      });
+      let res = await newReservedInfo.save();
+      response = this.formatDataSuccess('报名成功！');
+    } catch(e){
+      response = this.formatDataFail(e.message);
+    }
+    return response;
+  }
 
 }
 
