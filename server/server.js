babd0bf30d78a5e8a84b9082e0b736c442674c68
reri_liv <reri_liv@163.com>
2019-12-08 23:52:23 +0800

    fallback

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const App = require(`${__dirname}/app/app`);
 const app = new App();
 
-app.listen(443, () => {
-  console.log('Server running at http://127.0.0.1:443');
+app.listen(9000, () => {
+  console.log('Server running at http://127.0.0.1:9000');
 });
